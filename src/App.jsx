20051f59d40666424a6526d364a248ac5287717a
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-05-08 11:07:27 +0600

    add user and get the value

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
 
 function App() {
+  const handleAddUser = event => {
+    // stop the reload 
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    // create a user
+    const user = { name, email };
+    console.log(user);
+  }
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
