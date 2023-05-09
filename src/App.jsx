97f34a0492f800bf56fa64d4ffb4a9e30ba56ebd
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-05-09 10:30:44 +0600

    send the data in the mongodb server

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,20 @@ function App() {
     const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
-    // create a user
+    // create a new user
     const user = { name, email };
     console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
   }
   return (
     <>
